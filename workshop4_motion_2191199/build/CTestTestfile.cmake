f6cb108a9c852299808711c89771704da7fc2290
Christos Skapetis <45330616+Skapis9999@users.noreply.github.com>
2025-06-11 19:00:09 +0200

    Add files via upload

---
+++ workshop4_motion_2191199/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jetbot/EVC/workshops/workshop4_motion_2191199/src
+# Build directory: /home/jetbot/EVC/workshops/workshop4_motion_2191199/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("motion_skapis")
---
+++ workshop4_motion_2191199/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/jetbot/EVC/workshops/workshop4_motion_2191199/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
